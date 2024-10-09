6e5ce4171eae34f765c2748987fcc500b17f1283
kashish-2874 <kashishbarthwal2874@gmail.com>
2024-10-09 13:37:14 +0530

    kashish

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,8 @@ import Navbar  from '@/components/navbar/Navbar'
 import About from '@/components/about/About'
 import Footer from '@/components/footer/Footer'
 import Home1 from '@/components/home1/Home1'
+import Home2 from '@/components/home2/page'
+import Home3 from '@/components/home3/page'
 
 
 const page = () => {
@@ -18,8 +20,11 @@ const page = () => {
       
         <Navbar/>
         <Home1/>
+        <Home2 />
+        <Home3 />
         <About/>
         <Footer/>
+        
 
 
 
--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+// import React from 'react'
+import Image from "next/image";
 import styles from "./about.module.css" ; 
 
 
 const About = () => {
   return (
-    <div>
-      
+    <div className={styles.container}>
+      <hr />
+      <div className={styles.text}>
+        <h1>About Us</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis fugit nostrum optio magni, rem voluptatibus ipsam hic ea delectus veritatis tenetur adipisci ab distinctio alias quibusdam cum! Libero, eius ab.
+        Ipsa beatae dolorum nobis suscipit doloremque nihil repudiandae, quo officiis nesciunt eos ducimus delectus dolorem odio laudantium impedit placeat distinctio. Distinctio amet quisquam eos perferendis eaque suscipit culpa consequuntur debitis!</p>
+        <br />
+        <button>More About Us </button>
+        <Image src="/i.svg" alt="" width={0} height={0} className={styles.arrow} />
+      </div>
     </div>
   )
 }
