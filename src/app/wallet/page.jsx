401fbd2841523b596c4bb78133b8610243dc3929
Lokesh <lokeshgargadv@gmail.com>
2024-10-09 23:40:56 +0530

    Update

--- src/app/wallet/page.jsx
+++ src/app/wallet/page.jsx
@@ -1,27 +1,33 @@
-import React from 'react'
-import styles from "./wallet.module.css"
-import Image from "next/image" ; 
+import React from "react";
+import styles from "./wallet.module.css";
+import Image from "next/image";
+import { createThirdwebClient } from "thirdweb";
+import { ConnectButton } from "thirdweb/react";
 
 const WalletPage = () => {
+  // const client = createThirdwebClient({ secretKey: process.env.THIRDWEB_KEY });
+
   return (
     <div className={styles.container}>
       <div className={styles.logo_container}>
-        <Image src="/logo.svg" alt="logo" width={0} height={0} className={styles.logo_img}/>
+        <Image
+          src="/logo.svg"
+          alt="logo"
+          width={0}
+          height={0}
+          className={styles.logo_img}
+        />
       </div>
-      <div className={styles.content}>
-        Connect your wallet !
-      </div>
+      <div className={styles.content}>Connect your wallet !</div>
       <div className={styles.button}>
         <button>Connect</button>
+        {/* <ConnectButton client={client} /> */}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default WalletPage ;
-
-
-
+export default WalletPage;
 
 // import React, { useEffect } from 'react';
 // import Image from 'next/image'; // This is how Next.js handles images
@@ -43,12 +49,12 @@ export default WalletPage ;
 //     <div className={styles.container}>
 //       {/* Using Next.js's Image component for optimized images */}
 //       <Image src="/logo.svg" alt="logo" width={200} height={200} className={styles.logo_img} />
-      
+
 //       {/* Content initially hidden */}
 //       <div className={styles.content}>
 //         <h1>Welcome to the website</h1>
 //       </div>
-      
+
 //       {/* Button initially hidden */}
 //       <button className={styles.buttotn}>Click Me</button>
 //     </div>
