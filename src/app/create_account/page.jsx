283bc6b7f6464da4a2403f587490a8138a5e62ac
kashish-2874 <kashishbarthwal2874@gmail.com>
2024-10-13 22:08:25 +0530

    adding create account page with css file

--- src/app/create_account/page.jsx
+++ src/app/create_account/page.jsx
@@ -6,11 +6,14 @@ const CreateAccountPage = () => {
   return (
     <div className={styles.container}>
       <div className={styles.img}>
-        <Image src="/standing.gif" alt="" height={650} width={700} />
+      <Image src="/standing.gif" alt="Standing gif" width={0} height={0} layout="responsive" className={styles.image} />
       </div>
       <div className={styles.form}>
-        <input type="text" placeholder='enter your username' />
-        <button>Create</button>
+        <div className={styles.heading}><h1>Join Us Today! Create Your Account and Start Exploring</h1></div>
+        <div className={styles.input}>
+          <input type="text" placeholder='enter your username' />
+          <button>Create</button>
+        </div>
       </div>
     </div>
   )
