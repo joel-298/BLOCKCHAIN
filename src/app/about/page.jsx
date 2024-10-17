221cb94285e70985f73fb8a050eb83e1d65b3c16
kashish-2874 <kashishbarthwal2874@gmail.com>
2024-10-17 10:55:00 +0530

    about page completed

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,6 +3,7 @@ import Navbar from '@/components/navbar/Navbar';
 import React from 'react'
 import styles from './about.module.css'
 import Image from 'next/image';
+import Link from 'next/link';
 
 const AboutPage = () => {
   return (
@@ -26,20 +27,48 @@ const AboutPage = () => {
         <div className={styles.heading2}><h1 className={styles.h1}>Our Team</h1></div>
         <div className={styles.team}>
           <div className={styles.box}>
-            <Image src="/noavatar.png" alt="profile_img" height={0} width={0} className={styles.img} />
-            <p className={styles.p}>Joel</p>
+            <div className={styles.one}>
+                <Image src="/noavatar.png" alt="profile_img" height={0} width={0} className={styles.img} />
+                <p className={styles.p}>Lokesh</p>
+              </div>
+
+              <div className={styles.two}>
+                <Link href="/" passHref><Image src="/linkedin.svg" alt="" height={0} width={0} className={styles.icon}  /></ Link>
+                <Link href="/" passHref><Image src="/github.svg" alt="" height={0} width={0} className={styles.icon} /></Link>
+              </div>
           </div>
           <div className={styles.box}>
-            <Image src="/noavatar.png" alt="profile_img" height={0} width={0} className={styles.img} />
-            <p className={styles.p}>Kashish</p>
+            <div className={styles.one}>
+                <Image src="/noavatar.png" alt="profile_img" height={0} width={0} className={styles.img} />
+                <p className={styles.p}>Kavyam</p>
+              </div>
+
+              <div className={styles.two}>
+                <Link href="/" passHref><Image src="/linkedin.svg" alt="" height={0} width={0} className={styles.icon}  /></ Link>
+                <Link href="/" passHref><Image src="/github.svg" alt="" height={0} width={0} className={styles.icon} /></Link>
+              </div>
           </div>
           <div className={styles.box}>
-            <Image src="/noavatar.png" alt="profile_img" height={0} width={0} className={styles.img} />
-            <p className={styles.p}>Kavyam</p>
+            <div className={styles.one}>
+                <Image src="/noavatar.png" alt="profile_img" height={0} width={0} className={styles.img} />
+                <p className={styles.p}>Joel</p>
+              </div>
+
+              <div className={styles.two}>
+                <Link href="https://www.linkedin.com/in/joel-matthew-b40394258/" passHref target='/'><Image src="/linkedin.svg" alt="" height={0} width={0} className={styles.icon}  /></ Link>
+                <Link href="https://github.com/joel-298" passHref target='/'><Image src="/github.svg" alt="" height={0} width={0} className={styles.icon} /></Link>
+              </div>
           </div>
           <div className={styles.box}>
-            <Image src="/noavatar.png" alt="profile_img" height={0} width={0} className={styles.img} />
-            <p className={styles.p}>Lokesh</p>
+            <div className={styles.one}>
+              <Image src="/noavatar.png" alt="profile_img" height={0} width={0} className={styles.img} />
+              <p className={styles.p}>Kashish</p>
+            </div>
+
+            <div className={styles.two}>
+              <Link href="https://www.linkedin.com/in/kashish-barthwal-875014280/" passHref target='/'><Image src="/linkedin.svg" alt="" height={0} width={0} className={styles.icon}  /></ Link>
+              <Link href="https://github.com/kashish-2874" passHref target='/'><Image src="/github.svg" alt="" height={0} width={0} className={styles.icon} /></Link>
+            </div>
           </div>
         </div>
       </div>  
